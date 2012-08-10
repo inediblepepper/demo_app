d59d7617894e1271c74dea38874dca39ba189b5f
Steve Pepper <spepper@salesforce.com>
2012-08-10 17:12:41 -0600

    First demo app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'e7d17cfb1e257e495afc4ef75fa9dcfd5fee53d49ccc564a0c32798f02281c3c1ce528b5fc94ed1112e4ecdc01888f6e4ff6902971c786f9a1501dd585322fce'
